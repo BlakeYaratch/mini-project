cc951a1f493b672f19c9061b64bbbe70699f73c3
byaratch <yaratch_blake@student.mahoningctc.com>
2021-11-04 10:12:09 -0400

    fith

--- mini project/Program.cs
+++ mini project/Program.cs
@@ -22,21 +22,21 @@ namespace mini_project
             char2.speed = 4;
             char2.dexterity = 3;
             char2.intelligence = 10;
-            Console.WriteLine($"The first character you have made has the name of {char2.name}. His stats are as follows: Speed:{char2.speed}   Dexterity:{char2.dexterity}    Intelligence:{char2.intelligence}");
+            Console.WriteLine($"The second character you have made has the name of {char2.name}. His stats are as follows: Speed:{char2.speed}   Dexterity:{char2.dexterity}    Intelligence:{char2.intelligence}");
 
             Character_Blueprint char3 = new Character_Blueprint();
             char3.name = "Steven";
             char3.speed = 4;
             char3.dexterity = 3;
             char3.intelligence = 10;
-            Console.WriteLine($"The first character you have made has the name of {char3.name}. His stats are as follows: Speed:{char3.speed}   Dexterity:{char3.dexterity}    Intelligence:{char3.intelligence}");
+            Console.WriteLine($"The third character you have made has the name of {char3.name}. His stats are as follows: Speed:{char3.speed}   Dexterity:{char3.dexterity}    Intelligence:{char3.intelligence}");
 
             Character_Blueprint char4 = new Character_Blueprint();
             char4.name = "Steven";
             char4.speed = 4;
             char4.dexterity = 3;
             char4.intelligence = 10;
-            Console.WriteLine($"The first character you have made has the name of {char4.name}. His stats are as follows: Speed:{char4.speed}   Dexterity:{char4.dexterity}    Intelligence:{char4.intelligence}");
+            Console.WriteLine($"The fourth character you have made has the name of {char4.name}. His stats are as follows: Speed:{char4.speed}   Dexterity:{char4.dexterity}    Intelligence:{char4.intelligence}");
 
             Wizard char5 = new Wizard();
             char5.name = "Steven";
@@ -44,7 +44,7 @@ namespace mini_project
             char5.dexterity = 3;
             char5.intelligence = 10;
             char5.hasFireBall = true;
-            Console.WriteLine($"The first character you have made has the name of {char5.name}. His stats are as follows: Speed:{char5.speed}   Dexterity:{char5.dexterity}    Intelligence:{char5.intelligence}. Has Fireball? {char5.hasFireBall}.");
+            Console.WriteLine($"The fith character you have made has the name of {char5.name}. His stats are as follows: Speed:{char5.speed}   Dexterity:{char5.dexterity}    Intelligence:{char5.intelligence}. Has Fireball? {char5.hasFireBall}.");
             char5.woosh();
             
             Console.ReadKey();
